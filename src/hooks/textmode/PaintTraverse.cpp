fe41b2d1f775b539a9567eade929bfeab9070444
NW-Lightweight <tf2gym@gmail.com>
2024-03-02 14:56:34 +0100

    1313

--- src/hooks/textmode/PaintTraverse.cpp
+++
@@ -1,9 +0,0 @@
-#include "common.hpp"
-#include "HookedMethods.hpp"
-namespace hooked_methods
-{
-DEFINE_HOOKED_METHOD(PaintTraverse, void, vgui::IPanel *, unsigned int, bool, bool)
-{
-    return;
-}
-} // namespace hooked_methods
